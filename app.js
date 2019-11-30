137691e66ed31cbc1de46fb9eb50eceeac3b1d70
jerome <jerome.henry3@gmail.com>
2019-11-30 17:19:48 +0100

    added headers to allow cors

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Middleware ajoutant le header pour les CORS en local
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
+
 app.use(isAuth); // On passe le middleware
 
 app.use('/graphql', graphQlHttp({
